7a1d588f807fa173276165328c8999e6ea9d95ac
Christian OKeeffe <christian@joedal.dk>
2014-09-15 11:20:09 +0200

    Translation added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
 (function(){
-  var app = angular.module('smartgridgame', []);
-  
+	
+  var app = angular.module('smartgridgame', ['pascalprecht.translate']);
+  app.config(['$translateProvider', function($translateProvider) {
+  	// add translation table
+  	$translateProvider.useStaticFilesLoader({
+            prefix: './translations/',
+            suffix: '.json'
+        });
+    $translateProvider.preferredLanguage('en');
+	}]);
 })();
