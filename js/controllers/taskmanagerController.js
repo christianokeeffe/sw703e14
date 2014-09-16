fde92e5262f47751b5920e8943733661e7ca8cdf
Aleksander <aleksandersn@gmail.com>
2014-09-16 11:17:48 +0200

    +templates, new version of UI and worked on modal

--- js/controllers/taskmanagerController.js
+++ js/controllers/taskmanagerController.js
@@ -1,10 +1,46 @@
 var myApp = angular.module('smartgridgame');
 
-myApp.controller('taskmanagerController', ['$scope', function($scope) {
-	$scope.selected = "Select a task..";
-    $scope.lifeOfAction = ['bake', 'make'];
+var ModalDemoCtrl = function ($scope, $modal, $log) {
 
-	$scope.update = function (action){
-    	$scope.selected = action;
-    }
-}]);
+  $scope.items = ['item1', 'item2', 'item3'];
+  $scope.open = function (selectedAction) {
+
+    var modalInstance = $modal.open({
+      templateUrl: 'myModalContent.html',
+      controller: ModalInstanceCtrl,
+      size: 'sm',
+      resolve: {
+        items: function () {
+          return $scope.items;
+        },
+        selectedAction: function(){
+        	return selectedAction;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+};
+
+
+var ModalInstanceCtrl = function ($scope, $modalInstance, items, selectedAction) {
+
+  $scope.items = items;
+  $scope.header = selectedAction;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $modalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+};
