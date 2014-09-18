583dcf3ffada29032bf2d22389dc89249488d91c
Aleksander <aleksandersn@gmail.com>
2014-09-18 15:41:04 +0200

    tighting up the modal controller

--- js/controllers/taskTableController.js
+++ js/controllers/taskTableController.js
@@ -1,9 +1,48 @@
 var myApp = angular.module('smartgridgame');
 
-myApp.controller('taskTableController', ['$scope', function($scope){
+myApp.controller('taskTableController', ['$scope','$modal', function ($scope,$modal){
 	$scope.tableActionContent=['Washer','Owen','Car','Dryer'];
 	
 	$scope.tableActionHeadA="Items";
 	$scope.tableActionHeadB="Activate";
 
+	$scope.selected = "default";
+
+	$scope.open = function (selectedAction) {
+
+    var modalInstance = $modal.open({
+      templateUrl: 'myModalContent.html',
+      controller: actionModalController,
+      size: 'sm',
+      resolve: {
+        tableActionContent: function () {
+          return $scope.tableActionContent;
+        },
+        selectedAction: function(){
+        	return selectedAction;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    });
+  };
+
 }]);
+
+var actionModalController = function ($scope, $modalInstance, tableActionContent, selectedAction) {
+	$scope.items = tableActionContent;
+  	$scope.header = selectedAction;
+  	$scope.selected = {
+    	item: $scope.items[0]
+  	};
+
+  	$scope.ok = function () {
+    	$modalInstance.close($scope.selected.item);
+  	};
+
+  	$scope.cancel = function () {
+    	$modalInstance.dismiss('cancel');
+  	};
+};
