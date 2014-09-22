5e0a2380b95678cd15b9653d52623a5ba411a6de
Niels <niels@lichking.dk>
2014-09-22 13:05:07 +0200

    done so priceTable now use json

--- js/controllers/priceTableController.js
+++ js/controllers/priceTableController.js
@@ -1,30 +1,47 @@
 var myApp = angular.module('smartgridgame');
 
-myApp.controller('priceTableController', ['$scope', function($scope){
+myApp.controller('priceTableController', ['$scope', '$translate', function($scope, $translate){
+
 	$scope.content = {
 		"data": [
 			{
-				"price":"4",
-				"name":"Wind"
+				"price" : "5",
+				"name": "wind"
 			},
 			{
-				"price":"5",
-				"name":"Sun"
+				"price":"4",
+				"name":"sun"
 			},
 			{
 				"price":"12",
-				"name":"Coal"
+				"name":"coal"
 			},
 			{
 				"price":"11",
-				"name":"BIO"
+				"name":"bio"
 			}
-
 		]
-	}
-
+	};
 	
-	$scope.findHighestPrice = function(){
+	// Translate the names for the energy sources
+	$translate('textToPriceTable.wind').then(function (translations){
+		$scope.content.data[0].name = translations;
+	});
+
+	$translate('textToPriceTable.sun').then(function (translations){
+		$scope.content.data[1].name = translations;
+	});
+
+	$translate('textToPriceTable.coal').then(function (translations){
+		$scope.content.data[2].name = translations;
+	});
+
+	$translate('textToPriceTable.bio').then(function (translations){
+		$scope.content.data[3].name = translations;
+	});
+	// End for translating
+
+	$scope.findLowestPrice = function(){
 		var HighestPrice = Number.MAX_VALUE;
 		for (var i = 0 ; i <= $scope.content.data.length-1; i++) {
 			if (parseInt($scope.content.data[i].price) < HighestPrice) {
@@ -33,9 +50,4 @@ myApp.controller('priceTableController', ['$scope', function($scope){
 		}
 		return HighestPrice;
 	};
-	$scope.tablePriceHighestPrice = "";
-	
-	$scope.tablePriceHeadA="Category";
-	$scope.tablePriceHeadB="Price";
-
 }]);
