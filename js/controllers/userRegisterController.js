77943e78f1a0bc36eb575c01381d29bc6463bb7f
Christian OKeeffe <christian@joedal.dk>
2014-10-03 13:24:04 +0200

    Added error when email dublicate

--- js/controllers/userRegisterController.js
+++ js/controllers/userRegisterController.js
@@ -2,6 +2,7 @@ var myApp = angular.module('smartgridgame');
 
 myApp.controller('userRegisterController', ['$scope','$interval','usersFactory','formatRequest', function($scope,$interval,usersFactory,formatRequest){
 	$scope.user = {};
+	$scope.invalidEmail = false;
 
 	$scope.regUser = function()
 	{
@@ -16,7 +17,17 @@ myApp.controller('userRegisterController', ['$scope','$interval','usersFactory',
 	  { 
 	    usersFactory.registerUser(params,
 	    function (response) {
-	        $scope.regUser = response.data;
+	    	switch(response.status_code)
+	    	{
+	    		case '200':
+	    			$scope.regUser = response.data;
+	    			$scope.invalidEmail = false;
+    				break;
+				case '409':
+					$scope.invalidEmail = true;
+					break;
+	    	}
+	        
 	    },
 	    function (response) {
 	        //alert(JSON.stringify(response));
