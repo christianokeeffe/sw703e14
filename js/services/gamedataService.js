bd0b18498d10317d1c13a27b8f7d92aed979d67b
Christian OKeeffe <christian@joedal.dk>
2014-10-06 13:55:18 +0200

    gamedata working

--- js/services/gamedataService.js
+++ js/services/gamedataService.js
@@ -3,11 +3,11 @@
 /* Services */
 var services = angular.module('smartgridgame');
 
-var api_resource_user = api_url + "/gamedata";
+var api_resource_gamedata = api_url + "/gamedata";
     	
 
 services.factory("gamedataFactory", function($resource) {
-    return $resource(api_resource_user + ":endurl", {}, {
+    return $resource(api_resource_gamedata + ":endurl", {}, {
         saveGameData : { method: 'PUT', isArray: false, params: {'publicKey': '@publicKey', 'request':'@request', 'requestHash':'@requestHash'}}
     });
 });
