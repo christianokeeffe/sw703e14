ee1ae791109bb59e09f8c85a51c1bf63b9ccf757
Christian OKeeffe <christian@joedal.dk>
2014-10-06 10:15:26 +0200

    Added balance

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -3,6 +3,8 @@ var myApp = angular.module('smartgridgame');
 myApp.controller('mainController', ['$scope','$interval', function($scope,$interval){
 	$scope.gameSecOnRealSec = 1800;
 	$scope.dateEpoch = 1409565600;
+	var timeSinceLastWeek = 1409565600;
+	$scope.balance = 0;
 
 	$scope.curDate = function(){
 		var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
@@ -12,5 +14,10 @@ myApp.controller('mainController', ['$scope','$interval', function($scope,$inter
 
 	var test = $interval(function(){
 		$scope.dateEpoch += $scope.gameSecOnRealSec;
+		if($scope.dateEpoch - timeSinceLastWeek >= 604800)
+		{
+			timeSinceLastWeek = timeSinceLastWeek + 604800;
+			$scope.balance += 500;
+		}
 		},1000);
 } ]);
