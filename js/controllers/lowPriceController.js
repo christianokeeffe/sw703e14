fdcb4fdc7fed416cebf28e6c53e85da97ade528b
Christian OKeeffe <christian@joedal.dk>
2014-10-27 14:06:08 +0100

    added duration to the lowprice modal

--- js/controllers/lowPriceController.js
+++ js/controllers/lowPriceController.js
@@ -3,12 +3,24 @@ var myApp = angular.module('smartgridgame');
 myApp.controller('lowPriceController', ['$scope', '$rootScope', '$modalInstance', 'controllerService', function($scope, $rootScope ,$modalInstance, controllerService) {
   $scope.header = controllerService.getAppliance().name;
   $scope.task = controllerService.getTask().name;
+  
   $scope.alertShown = false;
 
   $scope.close = function () {
     $scope.startGameTime();
     $modalInstance.dismiss("Closed");
   };
+
+  $scope.formatTime = function (time) {
+    return pad(Math.floor(time/3600),2)+":"+pad((time%3600),2);
+  };
+
+  function pad(num, size) {
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
+$scope.duration = $scope.formatTime(controllerService.getTask().executionTime);
 
   $scope.setTimer = function () {
     $scope.beforeTime = controllerService.getTimer();
