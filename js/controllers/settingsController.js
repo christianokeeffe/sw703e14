3d4108714ddd098686c8ab71a7b82219a4eb6f68
Christian OKeeffe <christian@joedal.dk>
2014-11-10 09:36:04 +0100

    added settings

--- js/controllers/settingsController.js
+++ js/controllers/settingsController.js
@@ -2,6 +2,8 @@ var myApp = angular.module('smartgridgame');
 
 myApp.controller('settingsController', ['$scope','$rootScope','settingsFactory','formatRequest', function($scope, $rootScope,settingsFactory,formatRequest){
 	$scope.settings = {};
+	$scope.allow_discharge;
+	$scope.dischargeVal;
 
 	$scope.getSettings = function()
 	{
@@ -19,9 +21,31 @@ myApp.controller('settingsController', ['$scope','$rootScope','settingsFactory',
 
 		  settingsPromise.$promise.then(function(response){
 		    $scope.settings = response.data;
+		    // the code you're looking for
+			// iterate over each element in the array
+			for (var i = 0; i < $scope.settings.length; i++){
+			  // look for the entry with a matching `code` value
+			  switch($scope.settings[i].prefname)
+			  {
+			  	case "allow_discharge":
+					$scope.allow_discharge = $scope.settings[i].value == "true";
+			  		break;
+		  		case "dischargeVal":
+		  			$scope.dischargeVal = parseInt($scope.settings[i].value)
+		  			break;
+			  }
+			}
 		  });
 		}
 	};
 
 	$scope.getSettings();
+
+
+	$scope.$watch('allowDischarge', function() {
+
+	});
+	$scope.$watch('dischargeVal', function() {
+
+	});
 }]);
--- js/services/settingsService.js
+++ js/services/settingsService.js
@@ -3,11 +3,16 @@
 /* Services */
 var services = angular.module('smartgridgame');
 
-var api_resource_setting = api_url + "/settings";
+var api_resource_setting = api_url + "/setting";
     	
 
 services.factory("settingsFactory", function($resource) {
-    return $resource(api_resource_setting + "/:userID:endurl", {}, {
+    return $resource(api_resource_setting + "s/:userID:endurl", {}, {
         getSettings : { method: 'GET', isArray: false}
+    });
+});
+services.factory("settingsPostFactory", function($resource) {
+    return $resource(api_resource_setting, {}, {
+        saveGameData : { method: 'POST', isArray: false, params: {'publicKey': '@publicKey', 'request':'@request', 'requestHash':'@requestHash'}}
     });
 });
