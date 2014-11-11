ec12961059f26b6cea70f1a5cac4da21cff2d624
Mette Pedersen <mettemuzen@hotmail.com>
2014-11-11 10:38:19 +0100

    fixed the car again!!! + more pay per week

--- js/controllers/dailyTaskController.js
+++ js/controllers/dailyTaskController.js
@@ -112,7 +112,7 @@ var updater = $scope.$watch('dateEpoch', function(){
         	dayOfLastUpdate = Math.floor(dayOfLastUpdate%7);
         	currentDay = Math.floor(currentDay%7);
  			
- 			if(hourOfLastUpdate < 6 && 6 <= currentHour && currentDay != 2 && currentDay !=3) {
+ 			if(hourOfLastUpdate < 5 && 5 <= currentHour && currentDay != 2 && currentDay !=3) {
  				if($rootScope.carBattery - 2 * $rootScope.carChange >= 0) {
  					$rootScope.score += parseInt($scope.dailyTasks[i].reward);
  					$scope.dailyTasks[i].done = true;
