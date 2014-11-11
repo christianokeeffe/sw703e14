e2b5ed1c62a97baa62fd0f468b942c9421837c6d
Kasper Plejdrup <kplejd11@student.aau.dk>
2014-11-11 10:08:21 +0100

    no userID for productService

--- js/services/productsService.js
+++ js/services/productsService.js
@@ -6,7 +6,7 @@ var services = angular.module('smartgridgame');
 var api_resource_products = api_url + "/products";
     	
 services.factory("productsFactory", function($resource) {
-    return $resource(api_resource_appliances + "/:userID:endurl", {}, {
+    return $resource(api_resource_appliances + ":endurl", {}, {
         getProducts : { method: 'GET', isArray: false},
         insertProducts : { method: 'PUT', isArray: false, params: {'publicKey': '@publicKey', 'request':'@request', 'requestHash':'@requestHash'}}
     });
