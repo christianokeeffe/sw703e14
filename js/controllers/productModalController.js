8f04dc7a7c33fca8cdd69dbb5e68d186f85fb08a
Kasper Plejdrup <kplejd11@student.aau.dk>
2014-11-13 15:23:59 +0100

    Changed modal

--- js/controllers/productModalController.js
+++ js/controllers/productModalController.js
@@ -1,31 +1,11 @@
 var myApp = angular.module('smartgridgame');
 
-myApp.controller('productModalController',['$scope', '$modalInstance', 'selectedProduct', 'powerUsage', 'powerCost', function($scope, $modalInstance, selectedProduct, powerUsage, powerCost){
+myApp.controller('productModalController',['$scope', '$modalInstance', 'selectedProduct', 'powerCost', function($scope, $modalInstance, selectedProduct, powerCost){
 	$scope.product = selectedProduct;
-	$scope.calculatedResult = 0;
-	$scope.times;
 
-	$scope.powerUsage = parseInt(powerUsage);
 	$scope.powerCost = parseFloat(powerCost);
-	$scope.productsProductionPerMonth = parseInt($scope.product.watt) / 12;
+	$scope.productsProduction = Math.floor(parseInt($scope.product.watt)*$scope.powerCost);
 
-	$scope.calculatePreview = function()
-	{
-		var result1 = $scope.powerUsage*$scope.times*$scope.powerCost;
-		console.log(result1);
-		var result2 = $scope.productsProductionPerMonth*$scope.times*$scope.powerCost;
-		console.log(result2);
-		$scope.calculatedResult = Math.floor(result1 - result2);
-	}
-
-	$scope.preformCheck = function(time){
-		if(time == undefined){
-			$scope.times = 0;
-		} else {
-			$scope.times = time;
-		}
-		$scope.calculatePreview();
-	}
 
 	$scope.buy = function(){
 		$modalInstance.close(selectedProduct);
